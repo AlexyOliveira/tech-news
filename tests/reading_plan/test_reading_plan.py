37bc362db934c6a58dcb15b0b54ea5f66ae92d65
alexyoliveira <cupofteaoficial@gmail.com>
2023-07-21 11:39:44 -0300

    req 6 done

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,15 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import (
+    ReadingPlanService,
+)  # noqa: F401, E261, E501
+import pytest
 
 
 def test_reading_plan_group_news():
-    pass
+    r = ReadingPlanService()
+    a = r.group_news_for_available_time(1)
+    with pytest.raises(ValueError):
+        r.group_news_for_available_time(0)
+    assert a["unreadable"] == [("Notícia bacana", 4)]
+    assert a["readable"] == [
+        {"unfilled_time": 0, "chosen_news": [("Notícia bacana 2", 1)]}
+    ]
