c24a0695899455378669b3d6c8f5b102433fe08e
alexyoliveira <cupofteaoficial@gmail.com>
2023-07-24 19:56:33 -0300

    req 7 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,20 @@
+from tech_news.database import db
+import re
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = []
+    regex_title = re.compile(title, re.IGNORECASE)
+    db_news = db.news.find(
+        {"title": regex_title}, {"_id": False, "title": True, "url": True}
+    )
+    for new in db_news:
+        news.append((new["title"], new["url"]))
+    return news
+
+
+# print(search_by_title("notícia bacana"))
 
 
 # Requisito 8
