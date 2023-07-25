5746484a4dd2adbf01d45e3ba10fedb4e42eb79c
alexyoliveira <cupofteaoficial@gmail.com>
2023-07-25 11:52:18 -0300

    req 8 and 9 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 import re
 
 
@@ -19,11 +20,40 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        news = []
+
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+
+        dd_mm_yyyy_format_date = date_object.strftime("%d/%m/%Y")
+
+        db_news = db.news.find(
+            {"timestamp": dd_mm_yyyy_format_date},
+            {"_id": False, "title": True, "url": True},
+        )
+
+        for new in db_news:
+            news.append((new["title"], new["url"]))
+        return news
+
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
+# print(search_by_date("2021"))
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = []
+    category_title = re.compile(category, re.IGNORECASE)
+    db_news = db.news.find(
+        {"category": category_title},
+        {"_id": False, "title": True, "url": True},
+    )
+    for new in db_news:
+        news.append((new["title"], new["url"]))
+    return news
+
+
+# print(search_by_date("tecnologia"))
