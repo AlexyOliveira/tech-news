acfb3153c8745d7042e5f651edb5a31208ec5b73
alexyoliveira <cupofteaoficial@gmail.com>
2023-07-25 12:20:48 -0300

    req 10 done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,11 @@
-# Requisito 10
+from tech_news.database import find_news
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = find_news()
+    category_counts = Counter(item["category"] for item in news)
+    top_categories = sorted(
+        category_counts.items(), key=lambda x: (-x[1], x[0])
+    )
+    return [category for category, _ in top_categories[:5]]
